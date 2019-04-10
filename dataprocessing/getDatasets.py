598a430a2597c8d84652a135aa897258b48a20b2
computational-physcis-2011301020083 <2432001232@qq.com>
2019-04-10 13:49:51 -0700

    Get datasets using rucio

---
+++ dataprocessing/getDatasets.py
@@ -0,0 +1,57 @@
+import os
+
+with open("/global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/dataprocessing/HbbDatasets1.csv", "rU") as f:
+    lines = f.readlines()
+
+sourceDatasetsWithFlags = [x.strip() for x in lines]
+
+for sourceDatasetWithFlag in sourceDatasetsWithFlags:
+
+    sourceDatasetAndFlag = sourceDatasetWithFlag.split(",")
+
+    sourceDataset = sourceDatasetAndFlag[0]
+    flag = sourceDatasetAndFlag[1]
+
+    dsid = sourceDatasetWithFlag.split(".")[2]
+
+    print "processing " +  sourceDataset
+    output = os.popen("rucio list-file-replicas " + sourceDataset +" --rse NERSC_LOCALGROUPDISK")
+    
+    for line in output.readlines():
+        if "/projecta/projectdirs/atlas/" in line:
+              filepath = "/projecta/" + line.split("projecta/")[1].split(" |")[0]
+              if flag=="N":
+                dirname="/global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/DijetsDatasets"
+                os.popen("cp " + filepath + "   /global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/DijetsDatasets")
+                #os.popen("cp " + filepath + " ./hbbDijetsDatasets/")
+        
+                oldFileName = filepath.split("/")[-1]
+                newFileName = oldFileName.split('.h5')[0] + "_" + dsid + "_" + flag + ".h5"
+
+                os.popen("mv " +dirname+"/"+ oldFileName + "   "+dirname+"/"+ newFileName)
+
+              if flag=="T":
+                dirname="/global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/TopDatasets"
+                os.popen("cp " + filepath + "    /global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/TopDatasets")
+                #os.popen("cp " + filepath + " ./hbbDijetsDatasets/")
+        
+                oldFileName = filepath.split("/")[-1]
+                newFileName = oldFileName.split('.h5')[0] + "_" + dsid + "_" + flag + ".h5"
+               
+                os.popen("mv " +dirname+"/"+ oldFileName + "   "+dirname+"/"+ newFileName)
+                #os.popen("mv ./hbbDijetsDatasets/" + oldFileName + " ./hbbDijetsDatasets/" + newFileName)
+
+              if flag=="H":
+                dirname="/global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/HbbDatasets"
+                os.popen("cp " + filepath + "    /global/project/projectdirs/atlas/massDecorrelatedXbb/adversarial-wei1/datasets/HbbDatasets")
+                #os.popen("cp " + filepath + " ./hbbDijetsDatasets/")
+        
+                oldFileName = filepath.split("/")[-1]
+                newFileName = oldFileName.split('.h5')[0] + "_" + dsid + "_" + flag + ".h5"
+
+                #os.popen("mv ./hbbDijetsDatasets/" + oldFileName + " ./hbbDijetsDatasets/" + newFileName)
+                os.popen("mv " +dirname+"/"+ oldFileName + "   "+dirname+"/"+ newFileName)
+
+
+
+
